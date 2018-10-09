b0fff37b7ad59217ea6cc717accbb8c9c3b1ae6b
Michael Cruz <michaelcruz@Michaels-MacBook-Pro.local>
2018-10-09 17:02:28 -0300

    script to map bus stop

---
+++ bus_stop_script.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import preprocess as prep
+from tqdm import tqdm
+import numpy as np
+import gmplot.gmplot
+
+stops_data = pd.read_csv('GTFSjaneiro/stops.txt')
+
+data = pd.read_csv('data-100000.csv',delimiter=',', float_precision=None)
+
+data['label'] = ['in_route' for i in range(data.shape[0])]
+
+prepro = prep.PreProcess()
+
+mapped_stops = dict()
+for idx, row in tqdm(data.iterrows()):
+    if row.velocidade < 5:
+        for idx2,stop in stops_data.iterrows():
+            dist = prepro.distance_in_meters([row.lat,row.lng], [stop.stop_lat,stop.stop_lon])
+            if dist < 20:
+                data.loc[idx,'label'] = 'bus_stop'
+
+data.to_csv('data-100000-bus-stop-mapped.csv')
